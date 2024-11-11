8c3478e426e3a8809bdc6ddd232d5e8df3d86c1c
geovannarqzsbc07 <141033944+geovannarqzsbc07@users.noreply.github.com>
2024-11-11 09:51:46 -0300

    Update quantidadeUsuario.js

--- quantidadeUsuario.js
+++ quantidadeUsuario.js
@@ -21,6 +21,13 @@ async function quantidadeUsuariosPorRede() {
 const layout = {
     plot_bgcolor: getCSS('--bg-color')
     paper_bgcolor: getCSS('--bg-color')
+    title: {
+    text: 'Redes sociais com mais usuários no mundo'
+    x: 0,
+     font: {
+         color: getCSS('--primary-color'),
+         family: getCSS('--font'),
+         size: 30
 }
 
     const grafico = document.createElement('div')
